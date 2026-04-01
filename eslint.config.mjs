61ccfc6cd8a3f477cbb6264f99b8536148b075b9
Kristjan ESPERANTO <35647502+KristjanESPERANTO@users.noreply.github.com>
2026-04-01 19:51:41 +0200

    chore: remove demo.config.js rule from ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -45,6 +45,5 @@ export default defineConfig([
       "sort-keys": "off"
     }
   },
-  {files: ["demo.config.js"], rules: {"prefer-const": "off"}},
   {files: ["**/*.md"], plugins: {markdown}, language: "markdown/gfm", extends: ["markdown/recommended"]}
 ]);
